a0c2318bf2551d90f6000a312e58cb6cdae6a206
Matt Caldwell <matt.caldwell@gmail.com>
2013-02-14 06:02:31 -0500

    add setup.py for pypi

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fabness',
+      version='1.0',
+      description='Fabness dependency injection build framework.',
+      author='Matt Caldwell',
+      author_email='matt.caldwell@gmail.com',
+      url='https://github.com/mattcaldwell/fabness',
+      packages=['fabness']
+)
